8ffcf9b697e5421d7dd0afaf9eb6b34d443afe30
Sadman Kazi <sadmansazidk@gmail.com>
2014-11-02 03:44:48 -0500

    Fixed the entire screen rendering every frame, added dynamically available variables to the main function

--- source/main.cpp
+++ source/main.cpp
@@ -5,47 +5,62 @@
 #include <GL\glew.h>
 #include <GL\freeglut.h>
 
+//constant values
+const int DIM = 3, FPS = 24;
+const int WIDTH = 1000, HEIGHT = 500;
+const float scaleA = 1.0, scaleV = 1.0, scaleS = 1.0, armLength = 2.0; //stores the  armlength of the person using the program
+
 void map(float *pos, float qx, float qy, float qz, float arm) {
 	pos[0] = pos[0] + (qx * arm);
 	pos[1] = pos[1] + (qy * arm);
 	pos[2] = pos[2] + (qz * arm);
 }
 
-void draw(float x, float y) {
-	//glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(255, 255, 255, 255);
+//detects the stablity on the z position vector
+void onPaper(float z) {
 
+}
+
+void draw(float x, float y) {
+	//sets up the color for clearing the screen
+	//glClearColor(1.0f, 1.0f, 1.0f, 0);
+	//glClear(GL_COLOR_BUFFER_BIT);
+	//sets up the screen coordinates
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-60, 60, -60, 60, -1, 1);
+	glOrtho(-30, 30, -30, 30, -1, 1);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glColor3ub(68, 8, 45);
+	glColor3ub(0, 0, 0);
 	//glBegin(GL_LINE_STRIP);
-	glPointSize(10.0);
+	glPointSize(5.0);
 	glBegin(GL_POINTS);
 	glVertex2f(x, y);
 	glEnd();
 	glutSwapBuffers();
-
+	glReadBuffer(GL_FRONT);
+	glDrawBuffer(GL_BACK);
+	glCopyPixels(0, 0, WIDTH, HEIGHT, GL_COLOR);
 }
 
 int main(int argc, char** argv)
 {
-	const int dim = 3, timeInt = 500000;
-	float scaleA = 1.0, scaleV = 1.0, scaleS = 1.0;
+	
+	//int timeInt = 1000000 / FPS;
 	//for storing the accelerometer data and the position vector data
-	float *accelIn = (float*)malloc(dim * sizeof(float));
-	float *position = (float*)malloc(dim * sizeof(float));
-	float armLength = 2.0; //stores the  armlength of the person using the program
-
+	float *accelIn = (float*)malloc(DIM * sizeof(float));
+	float *position = (float*)malloc(DIM * sizeof(float));
+	
 	//set up the GUI
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutInitWindowSize(600, 600);
-	glutCreateWindow("GLUT");
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Draw");
+	glClearColor(1.0f, 1.0f, 1.0f, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
 	bool write = true;
 	
 	// We catch any exceptions that might occur below -- see the catch statement for more details.
@@ -72,7 +87,7 @@ int main(int argc, char** argv)
 
 		// Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
 		Filter collector;
-		Kinematic accToPos(dim, scaleA, scaleV, scaleS); //adds the integral class 
+		Kinematic accToPos(DIM, scaleA, scaleV, scaleS); //adds the integral class 
 		//sets the precision for floating points
 		std::cout.precision(2);
 
@@ -81,7 +96,7 @@ int main(int argc, char** argv)
 		// loop keeps running and mapping the coordinates on the window
 		while (true) {
 			//gets 48 packets of data every second
-			hub.run(1000 / 2);
+			hub.run(1000 / FPS);
 
 			//store the accelerometer data in an array
 			accelIn[0] = collector.accelX;
@@ -90,23 +105,20 @@ int main(int argc, char** argv)
 
 			//integrate the data
 			position = accToPos.update(accelIn);
-
-			//send the value to map
+							//send the value to map
 			map(position, collector.quatX, collector.quatY, collector.quatZ, armLength);
 			std::cout << '\r';
 			if (write)
-				draw (position[0], position[1]);
+				draw(position[0] * 10, position[1] * 10);
 			//print the position
-			for (int i = 0; i < dim; i++)
+			for (int i = 0; i < DIM; i++)
 				std::cout << accelIn[i] << " ";
-
-		}
-
+			}
 		free(accelIn);
 		free(position);
 		accToPos.freeAll();
-		// If a standard exception occurred, we print out its message and exit.
 	}
+	// If a standard exception occurred, we print out its message and exit.
 	catch (const std::exception& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		std::cerr << "Press enter to continue.";
